2b7ddd65f4ef276ea61ad2c0cfa84149f1c44e31
antonorlov <fogelo@yandex.ru>
2022-05-14 23:41:06 +0300

    make dark and ligh mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import Sidebar from "./Components/Sidebar";
 import HomePage from "./Pages/HomePage";
@@ -8,10 +8,25 @@ import ResumePage from "./Pages/ResumePage";
 import PortfoliosPage from "./Pages/PortfoliosPage";
 import BlogsPage from "./Pages/BlogsPage";
 import ContactsPage from "./Pages/ContactsPage";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import {Switch} from "@mui/material";
 
 function App() {
+    const [theme, setTheme] = useState<"dark-theme" | "light-theme">("dark-theme")
+    useEffect(() => {
+        document.documentElement.className = theme
+    }, [theme])
     return (
         <AppStyled>
+            <div className={"light-dark-mode"}>
+                {theme === "dark-theme"
+                    ? <LightModeOutlinedIcon/>
+                    : <LightModeIcon/>}
+                <Switch onChange={() => setTheme(theme === "dark-theme" ? "light-theme" : "dark-theme")}
+                        color="default"
+                />
+            </div>
             <Sidebar/>
             <MainContentStyled>
                 <div className={"lines"}>
@@ -21,12 +36,12 @@ function App() {
                     <div className="lines-4"/>
                 </div>
                 <Routes>
-                    <Route path={'/'} element={<HomePage/>}/>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/resume'} element={<ResumePage/>}/>
-                    <Route path={'/portfolios'} element={<PortfoliosPage/>}/>
-                    <Route path={'/blogs'} element={<BlogsPage/>}/>
-                    <Route path={'/contacts'} element={<ContactsPage/>}/>
+                    <Route path={"/"} element={<HomePage/>}/>
+                    <Route path={"/about"} element={<AboutPage/>}/>
+                    <Route path={"/resume"} element={<ResumePage/>}/>
+                    <Route path={"/portfolios"} element={<PortfoliosPage/>}/>
+                    <Route path={"/blogs"} element={<BlogsPage/>}/>
+                    <Route path={"/contacts"} element={<ContactsPage/>}/>
                 </Routes>
             </MainContentStyled>
         </AppStyled>
@@ -36,6 +51,21 @@ function App() {
 
 const AppStyled = styled.div`
   background-color: var(--background-dark-color);
+  position: relative;
+
+  .light-dark-mode {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 10%;
+    right: 0;
+    z-index: 1;
+  }
+
+  .MuiSwitch-track {
+    background-color: gray;
+  }
 `
 
 const MainContentStyled = styled.main`
@@ -53,7 +83,7 @@ const MainContentStyled = styled.main`
     .lines-1, .lines-2, .lines-3, .lines-4 {
       width: 1px;
       min-height: 100vh;
-      background-color: var( --border-color);
+      background-color: var(--border-color);
       opacity: .25;
     }
   }
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,24 +1,62 @@
 import {createGlobalStyle} from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-  :root {
+  //:root {
+  //  --primary-color: #007bff;
+  //  --secondary-color: #6c757d;
+  //  --background-dark-color: #10121a;
+  //  --background-dark-color-2: #191d2b;
+  //  --border-color: #2e344e;
+  //  --background-light-color: #f1f1f1;
+  //  --background-light-color-2: rgba(3, 127, 255, .3);
+  //  --white-color: #fff;
+  //  --font-light-color: #a4acc4;
+  //  --font-dark-color: #313131;
+  //  --font-dark-color-2: #151515;
+  //  --sidebar-dark-color: #191d2b;
+  //  --scrollbar-bg-color: #191d2b;
+  //  --scrollbar-thump-color: #191d2b;
+  //  --scrollbar-track-color: #191d2b;
+  //}
+
+
+  .light-theme{
     --primary-color: #007bff;
+    --primary-color-light: #057FFF;
+    --secondary-color: #ff7675;
+    --background-dark-color: #F1F1F1;
+    --background-dark-grey: #e4e4e4;
+    --border-color: #cbced8;
+    --background-light-color: #F1F1F1;
+    --background-light-color-2: rgba(3,127,255,.3);
+    --white-color: #151515;
+    --font-light-color: #313131;
+    --font-dark-color: #313131;
+    --font-dark-color-2: #151515;
+    --sidebar-dark-color: #E4E4E4;
+    --scrollbar-bg-color: #383838;
+    --scrollbar-thump-color: #6b6b6b;
+    --scrollbar-track-color: #383838;
+  }
+  .dark-theme{
+    --primary-color: #007bff;
+    --primary-color-light: #057FFF;
     --secondary-color: #6c757d;
-    --background-dark-color: #10121a;
-    --background-dark-color-2: #191d2b;
+    --background-dark-color: #10121A;
+    --background-dark-grey: #191D2B;
     --border-color: #2e344e;
-    --background-light-color: #f1f1f1;
-    --background-light-color-2: rgba(3, 127, 255, .3);
-    --white-color: #fff;
+    --background-light-color: #F1F1F1;
+    --background-light-color-2: rgba(3,127,255,.3);
+    --white-color: #FFF;
     --font-light-color: #a4acc4;
     --font-dark-color: #313131;
     --font-dark-color-2: #151515;
-    --sidebar-dark-color: #191d2b;
-    --scrollbar-bg-color: #191d2b;
-    --scrollbar-thump-color: #191d2b;
-    --scrollbar-track-color: #191d2b;
+    --sidebar-dark-color: #191D2B;
+    --scrollbar-bg-color: #383838;
+    --scrollbar-thump-color: #6b6b6b;
+    --scrollbar-track-color: #383838;
   }
-
+  
   * {
     padding: 0;
     margin: 0;
